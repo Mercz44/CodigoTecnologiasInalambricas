5d01140e2c1b59a8e6196e18e3ec01905137003f
Mercz44 <160765093+Mercz44@users.noreply.github.com>
2024-10-29 18:48:30 -0600

    Add files via upload

---
+++ PracticaV3/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace PracticaV3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAlgo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtAlgo
+            // 
+            this.txtAlgo.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAlgo.Location = new System.Drawing.Point(104, 39);
+            this.txtAlgo.Name = "txtAlgo";
+            this.txtAlgo.Size = new System.Drawing.Size(313, 49);
+            this.txtAlgo.TabIndex = 0;
+            this.txtAlgo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(521, 450);
+            this.Controls.Add(this.txtAlgo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAlgo;
+    }
+}
+
---
+++ PracticaV3/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaV3
+{
+    public partial class Form1 : Form
+    {
+        System.IO.Ports.SerialPort Port;
+        bool IsClosed = false;
+        public Form1()
+        {
+            InitializeComponent();
+            Port = new System.IO.Ports.SerialPort();
+            Port.PortName = "COM8";
+            Port.BaudRate = 9600;
+            Port.ReadTimeout = 500;
+            try { Port.Open(); 
+            }catch(Exception ex) 
+            {
+
+            }
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Thread Hilo = new Thread(EscuchaSerial);
+            Hilo.Start();
+        }
+        private void EscuchaSerial()
+        {
+            while (!IsClosed) {
+                try
+                {
+                    string cadena = Port.ReadLine();
+                    txtAlgo.Invoke(new MethodInvoker(delegate
+                    {
+                        txtAlgo.Text = cadena;
+                    }));
+                 
+                }catch { }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            IsClosed = true;
+            if (Port.IsOpen) { 
+            Port.Close();}
+        }
+    }
+}
